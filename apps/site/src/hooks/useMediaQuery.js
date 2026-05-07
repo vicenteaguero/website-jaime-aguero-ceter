08dfe82b856714dbcf6f975ea23301615c66ff56
Vicente Aguero <vicenteaguerocl@gmail.com>
2026-05-07 20:39:39 +0700

    feat(hooks): ✨ add useMediaQuery hook

---
+++ apps/site/src/hooks/useMediaQuery.js
@@ -0,0 +1,15 @@
+import { useEffect, useState } from "react";
+
+export function useMediaQuery(query) {
+  const [matches, setMatches] = useState(false);
+
+  useEffect(() => {
+    const mq = window.matchMedia(query);
+    setMatches(mq.matches);
+    const handler = (event) => setMatches(event.matches);
+    mq.addEventListener("change", handler);
+    return () => mq.removeEventListener("change", handler);
+  }, [query]);
+
+  return matches;
+}
